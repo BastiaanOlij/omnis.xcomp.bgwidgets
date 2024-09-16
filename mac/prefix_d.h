484bd070750c2e5bbcbb17f3bdc2096d0cd5e194
Bastiaan Olij <bastiaan.olij@instinctsystems.com.au>
2024-09-16 13:46:28 +1000

    Update to studio 11

---
+++ mac/prefix_d.h
@@ -0,0 +1,9 @@
+#define TESTVER 0
+#define _DEBUG
+
+#define mach_o_unicode
+#define isunicode
+#define UNICODE
+#define _UNICODE
+
+#include "carbnuni.h"
---
+++ mac/prefix_r.h
@@ -0,0 +1,8 @@
+#define TESTVER 0
+
+#define mach_o_unicode
+#define isunicode
+#define UNICODE
+#define _UNICODE
+
+#include "carbnuni.h"
